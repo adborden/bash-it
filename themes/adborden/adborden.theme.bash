321c80dc088379229a384f09fb8675432c433863
Aaron Borden <aaron@goodeggs.com>
2014-09-11 11:51:50 -0700

    [theme] add stash count to git prompt

--- themes/adborden/adborden.theme.bash
+++ themes/adborden/adborden.theme.bash
@@ -4,6 +4,12 @@ SCM_THEME_PROMPT_SUFFIX="${blue})"
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 
+
+function git_prompt_info {
+  git_prompt_vars
+  echo -e "$SCM_PREFIX$SCM_BRANCH${orange}$SCM_GIT_STASH$SCM_STATE$SCM_SUFFIX"
+}
+
 function prompt_command() {
     PS1="${green}\u@\h ${blue}\w${reset_color}\\n$(scm_prompt_info)${blue} \$${reset_color} ";
 }

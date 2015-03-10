850bbf395bd478fc451d2271cdcb377692a4abfd
Aaron Borden <aaron@goodeggs.com>
2015-03-09 22:28:55 -0700

    Host-specific themes

--- themes/adborden/adborden.theme.bash
+++ themes/adborden/adborden.theme.bash
@@ -10,8 +10,14 @@ function git_prompt_info {
   echo -e "$SCM_PREFIX$SCM_BRANCH${orange}$SCM_GIT_STASH$SCM_STATE$SCM_SUFFIX"
 }
 
+host_color=${green}
 function prompt_command() {
-    PS1="${green}\u@\h ${blue}\w${reset_color}\\n$(scm_prompt_info)${blue} \$${reset_color} ";
+    PS1="${host_color}\u@\h ${blue}\w${reset_color}\\n$(scm_prompt_info)${blue} \$${reset_color} ";
 }
 
+if [[ -r ${HOME}/.host ]]; then
+    read host < ${HOME}/.host
+    host_theme="${BASH_IT}/themes/${BASH_IT_THEME}/${BASH_IT_THEME}.${host}.theme.bash"
+    [[ -r ${host_theme} ]] && source $host_theme
+fi
 PROMPT_COMMAND=prompt_command;
